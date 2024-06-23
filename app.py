82001aa504e38ef385c7e21588cf8ae47810f2a0
jakobharttung <57986373+jakobharttung@users.noreply.github.com>
2024-06-23 16:01:33 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,28 +1,13 @@
 import streamlit as st
-import anthropic
-# Streamlit app
-st.title('Investor Analyst App')
-import anthropic
+from openai import OpenAI
+client = OpenAI()
 
-client = anthropic.Anthropic(
-    # defaults to os.environ.get("ANTHROPIC_API_KEY")
-    api_key="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"
+completion = client.chat.completions.create(
+  model="gpt-3.5-turbo",
+  messages=[
+    {"role": "system", "content": "You are a poetic assistant, skilled in explaining complex programming concepts with creative flair."},
+    {"role": "user", "content": "Compose a poem that explains the concept of recursion in programming."}
+  ]
 )
 
-message = client.messages.create(
-    model="claude-3-5-sonnet-20240620",
-    max_tokens=1000,
-    temperature=0,
-    messages=[
-        {
-            "role": "user",
-            "content": [
-                {
-                    "type": "text",
-                    "text": "tell me short poem on maths\n"
-                }
-            ]
-        }
-    ]
-)
-st.write(message.content)
+st.write(completion.choices[0].message)
