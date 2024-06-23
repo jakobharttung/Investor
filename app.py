7c22ebc0cfbd9de47ed95383138c26a43d0a70f2
jakobharttung <57986373+jakobharttung@users.noreply.github.com>
2024-06-23 19:41:57 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 import streamlit as st
 import anthropic
+my_api_key st.secrets['ATHROPIC_API_KEY']
 client = anthropic.Anthropic(
     # defaults to os.environ.get("ANTHROPIC_API_KEY")
-  api_key = ""
+  api_key = my_api_key
 )
 message = client.messages.create(
     model="claude-3-5-sonnet-20240620",
