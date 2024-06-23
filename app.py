c31bb50ca3e9979372c7d5754bbe5dc80d16cc9f
jakobharttung <57986373+jakobharttung@users.noreply.github.com>
2024-06-23 16:46:33 +0200

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import anthropic
 client = anthropic.Anthropic(
     # defaults to os.environ.get("ANTHROPIC_API_KEY")
   api_key = "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"
-
+)
 message = client.messages.create(
     model="claude-3-5-sonnet-20240620",
     max_tokens=1000,
