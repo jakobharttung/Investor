732289f56022b0c56c348fc86df3f92eb6cadc28
jakobharttung <57986373+jakobharttung@users.noreply.github.com>
2024-06-24 12:31:33 +0200

    Update app3.py

--- app3.py
+++ app3.py
@@ -22,6 +22,7 @@ def get_tickers(company):
 
 def get_stock_data(tickers, period='5y'):
     data = {}
+    st.write(tickers)
     for ticker in tickers:
         st.write(ticker)
         stock = yf.Ticker(ticker)
